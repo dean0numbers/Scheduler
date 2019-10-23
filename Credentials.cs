4a7bd687def9e8d83a70f41a45c7d3daae0847f8
dean0numbers <49329590+dean0numbers@users.noreply.github.com>
2019-10-23 07:07:04 -0700

    Add files via upload

---
+++ Credentials.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Credentials
+/// 
+/// !! BE WARNED !!
+/// 
+/// The following is not the recommended practice for security. (i.e.) to include the
+/// Passcode to your email account within the code and should be changed to a 
+/// more secure method, if you plan on using this on a public network.
+/// 
+/// Since, this is being used on and internal private network, I am willing to
+/// accept the minimal risk this posses, with the intention of changing it in later
+/// releases.
+/// 
+/// </summary>
+public class Credentials
+{
+
+    const String eMailAddress = "your@email.address"; // your email address goes here
+    const String eMailPassPhrase = "your Pass Phrase goes here"; // Your password goes here
+
+    public Credentials()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+ 
+    public String getEmailAddress()
+    {
+        return eMailAddress;
+    }
+
+    public String getEmailPassPhrase()
+    {
+        return eMailPassPhrase;
+    }
+
+}
+}
---
+++ ProjectClasses.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+/// <summary>
+/// Summary description for ProjectClasses
+/// </summary>
+public class ProjectClasses
+{
+    public ProjectClasses()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+
+    public DataTable GetData(string conStr, string cmdSqlStr)
+    {
+        DataTable dataTable = new DataTable();
+        System.Configuration.Configuration rootwebconfig = System.Web.Configuration.WebConfigurationManager.OpenWebConfiguration("/Mohtisham");
+        System.Configuration.ConnectionStringSettings conSql;
+        conSql = rootwebconfig.ConnectionStrings.ConnectionStrings[conStr];
+        SqlConnection dBConnection = new SqlConnection(conSql.ToString());
+
+        try
+        {
+            using (SqlCommand sqlCmd = new SqlCommand(cmdSqlStr, dBConnection)
+            {
+                CommandType = CommandType.StoredProcedure
+            })
+            {
+                dBConnection.Open();
+                SqlDataAdapter sqlDa = new SqlDataAdapter(sqlCmd);
+                sqlDa.Fill(dataTable);
+            }
+        }
+        catch (System.Data.SqlClient.SqlException ex)
+        {
+            string msg = "Fetch Error:";
+            msg += ex.Message;
+            throw new Exception(msg);
+        }
+        finally
+        {
+            dBConnection.Close();
+        }
+        return dataTable;
+    }
+}
